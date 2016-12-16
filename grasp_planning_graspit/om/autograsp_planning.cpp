9d16476764d0fa219d756b808b30679c13c62def
belledon <mbelledonne@gmail.com>
2016-12-16 11:51:53 -0500

    Added a main call to autgrasp_planning to bypass current issues with QT

--- grasp_planning_graspit/om/autograsp_planning.cpp
+++ grasp_planning_graspit/om/autograsp_planning.cpp
@@ -68,7 +68,6 @@ void handler(int sig)
     exit(1);
 }
 
-
 std::vector<double> quickGrasp(
     std::string& objectFilename, 
     std::string& robotFilename,
@@ -82,42 +81,192 @@ std::vector<double> quickGrasp(
     PRINTMSG("Initializing GraspIt")
     std::string name = "ContactGetter"; 
     SHARED_PTR<GraspIt::GraspItSceneManager> graspitMgr(new GraspIt::GraspItSceneManagerHeadless());  
-    // SHARED_PTR<GraspIt::ContactGetter> cg(new GraspIt::ContactGetter(name, graspitMgr));
+    SHARED_PTR<GraspIt::ContactGetter> cg(new GraspIt::ContactGetter(name, graspitMgr));
     
-    // GraspIt::EigenTransform robotTransform;
-    // GraspIt::EigenTransform objectTransform;
-    // // We want to keep the object at the absolute origin
-    // robotTransform.setIdentity();
-    // objectTransform.setIdentity();
-    // robotTransform.translate(robPos);
-    // // Have to do this because Eigen::Quaternion isn't covered by pybind/eigen
-    //   // Eigen::Quaterniond q(2, 0, 1, -3); 
-    // Eigen::Quaterniond robRotQ(robRot[0], robRot[1], robRot[2], robRot[3]);
-    // robotTransform.rotate(robRotQ);
-    // // robotTransform.rotate(robRot);
+    GraspIt::EigenTransform robotTransform;
+    GraspIt::EigenTransform objectTransform;
+    // We want to keep the object at the absolute origin
+    robotTransform.setIdentity();
+    objectTransform.setIdentity();
+    robotTransform.translate(robPos);
+    // Have to do this because Eigen::Quaternion isn't covered by pybind/eigen
+      // Eigen::Quaterniond q(2, 0, 1, -3); 
+    Eigen::Quaterniond robRotQ(robRot[0], robRot[1], robRot[2], robRot[3]);
+    robotTransform.rotate(robRotQ);
+    // robotTransform.rotate(robRot);
     
-    // std::string robotName="Robot1";
-    // std::string objectName="Object1";
+    std::string robotName="Robot1";
+    std::string objectName="Object1";
 
-    // if (graspitMgr->loadRobot(robotFilename, robotName, robotTransform) != 0)
-    // {
-    //     PRINTERROR("Could not load robot");
+    if (graspitMgr->loadRobot(robotFilename, robotName, robotTransform) != 0)
+    {
+        PRINTERROR("Could not load robot");
         
-    // }
+    }
 
-    // if (graspitMgr->loadObject(objectFilename, objectName, true, objectTransform))
-    // {
-    //     PRINTERROR("Could not load object");
-    // }
+    if (graspitMgr->loadObject(objectFilename, objectName, true, objectTransform))
+    {
+        PRINTERROR("Could not load object");
+    }
     
     std::vector<double> dofs;// = cg->autoGrasp();
     // cg.reset();
     // graspitMgr->destroyCore();
-    signal(SIGSEGV, handler);
-    signal(SIGABRT, handler);
-    // PRINT_INIT_STD();
     
     return dofs;
+}
+
+boost::program_options::options_description getOptions()
+{
+    // Declare the supported options.
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+    ("help", "produce help message")
+    ("rob", boost::program_options::value<std::string>(), "filename for the robot file")
+    ("obj", boost::program_options::value<std::string>(), "filename for the object file")
+    ("rob_pos", boost::program_options::value<std::vector<float> >()->multitoken(), "Position of the object relative to the robot: Specify one x, y and z value.")
+    ("rob_rot", boost::program_options::value<std::vector<float> >()->multitoken(), "A 4d quaternion to specify robot rotation.");
+    return desc;
+}
+
+boost::program_options::variables_map loadParams(int argc, char ** argv)
+{
+    boost::program_options::options_description optDesc = getOptions();
+    boost::program_options::variables_map vm;
+    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(optDesc).style(
+        boost::program_options::command_line_style::unix_style ^ boost::program_options::command_line_style::allow_short).run(), vm);
+    boost::program_options::notify(vm);
+    return vm;
+}
+
+bool loadParams(int argc, char ** argv, 
+  std::string& objectFilename, 
+  std::string& robotFilename,
+  Eigen::Vector3d robPos,
+  std::std::vector<double> robRot)
+{
+  objectFilename.clear();
+  robotFilename.clear();
+
+  boost::program_options::variables_map vm;
+  try
+  {
+    vm = loadParams(argc, argv);
+  }
+  catch (std::exception const& e)
+  {
+      PRINTERROR("Exception caught: " << e.what());
+      return false;
+  }
+  catch (...)
+  {
+      PRINTERROR("Exception caught");
+      return false;
+  }
+  boost::program_options::options_description desc = getOptions();
+  if (vm.count("help"))
+  {
+      PRINTMSG(desc);
+      return false;
+  }
+  // Validate user input
+  if (!vm.count('obj'))
+  {
+    PRINTERROR("Must specify object file");
+    PRINTMSG(desc);
+    return false;
+  } 
+  if (!vm.count('rob'))
+  {
+    PRINTERROR("Must specify robot file");
+    PRINTMSG(desc);
+    return false;
+  }
+  if (!vm.count('rob_pos'))
+  {
+    PRINTERROR("Must specify robot position");
+    PRINTMSG(desc);
+    return false;
+  } 
+  if (!vm.count('rob_rot'))
+  {
+    PRINTERROR("Must specify robot rotation");
+    PRINTMSG(desc);
+    return false;
+  }
+  if (vm.count("rob") > 1)
+  {
+      PRINTERROR("You can only specify one robot at this stage.");
+      PRINTMSG(desc);
+      return false;
+  }
+
+  if (vm.count("obj") > 1)
+  {
+      PRINTERROR("You can only specify one object at this stage.");
+      PRINTMSG(desc);
+      return false;
+  }
+  // Assign valid user input
+  
+  if (vm.count("obj"))
+  {
+      objectFilename = vm["obj"].as<std::string>();
+      PRINTMSG("Object file is " << objectFilename);
+  }
+  if (vm.count("rob"))
+  {
+      robotFilename = vm["rob"].as<std::string>();
+      PRINTMSG("Robot file is " << robotFilename);
+  }
+  if (vm.count("rob_pos"))
+  {
+    std::vector<float> vals=vm["rob_pos"].as<std::vector<float> >();
+    if (vals.size()!=3)
+    {
+        PRINTERROR("Must specify 3 values for --rob_pos: x, y and z (specified "<<vals.size()<<")");
+        PRINTMSG(desc);
+    }
+    PRINTMSG("Using initial object pose "<<vals[0]<<", "<<vals[1]<<", "<<vals[2]);
+    robPos=Eigen::Vector3d(vals[0],vals[1],vals[2]);
+  }
+  if (vm.count("rob_rot"))
+  {
+    std::vector<float> vals=vm["rob_rot"].as<std::vector<float> >();
+    if (vals.size() != 4)
+    {
+      PRINTERROR("Must specify 4 values for --rob_rot: w, x, y, z (specified "<<vals.size()<<")");
+      PRINTMSG(desc);
+    }
+    rob_rot = vals;
+  }
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  signal(SIGSEGV, handler);
+  signal(SIGABRT, handler);
+
+  PRINT_INIT_STD();
+
+  std::string objectFilename;
+  std::string robotFilename;
+  Eigen::Vector3d robPos;
+  std::std::vector<double> robRot;
+
+  if (!loadParams(argc, argv, objectFilename, robotFilename, robPos, robRot))
+  {
+    PRINTERROR("Could not read arguments");
+    return 1;
+  }
+
+  PRINTMSG("Performing quickGrasp...");
+  std::std::vector<double> dofs = quickGrasp(objectFilename, robotFilename, robPos, robRot);
+  PRINTMSG("The resulting dofs are");
+  cout << dofs;
+  return 0;
+
 }
 
 namespace py = pybind11;
@@ -126,7 +275,7 @@ PYBIND11_PLUGIN(autograsp_planning) {
     py::module m("autograsp_planning", "Graspit!-quickGrasp plugin");
 
     m.def("quickGrasp", &quickGrasp, "Performs Graspit!-autograsp");
-
+    m.def("main", &main, "Main call to quickGrasp");
     return m.ptr();
 }
 
